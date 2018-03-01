0518b89d6cd040e29703d3356ee6f11642ec575a
Unknown <gaspard.thirion@epitech.eu>
2018-03-01 20:44:03 +0100

    Auto stash before merge of "master" and "origin/master"

--- include/Map.hpp
+++ include/Map.hpp
@@ -7,23 +7,29 @@
 
 #include <vector>
 #include <string>
+#include <map>
+#include <iostream>
 #include "Vehicle.hpp"
 
 class Map {
 
 public:
-    Map(const std::string &fileName);
-
+	Map(const std::string &fileName);
+	void HardInit();
+	void Resolve();
+	std::map<int, std::shared_ptr<Ride>> GetRidesByStart(int) {
+		return std::map<int, std::shared_ptr<Ride>>();
+	};
 private:
-    std::string                         _fileName;
-    std::vector<Vehicle>                _vehicles;
-    std::vector<std::shared_ptr<Ride>>  _rides;
+	std::string                         _fileName;
+	std::vector<Vehicle>                _vehicles;
+	std::vector<std::shared_ptr<Ride>>  _rides;
 
-    Vector2                 _gridSize;
-    unsigned int            _nbVehicle;
-    unsigned int            _nbRides;
-    unsigned int            _bonus;
-    unsigned long long int  _maxTime;
+	Vector2                 _gridSize;
+	unsigned int            _nbVehicle;
+	unsigned int            _nbRides;
+	unsigned int            _bonus;
+	unsigned long long int  _maxTime;
 };
 
 
--- sources/Map.cpp
+++ sources/Map.cpp
@@ -5,6 +5,23 @@
 #include "Map.hpp"
 
 Map::Map(const std::string &fileName) :
-        _fileName(fileName) {
+	_fileName(fileName) {
 
+}
+
+void Map::HardInit()
+{
+	//_gridSize = new Vector2(10, 10)
+}
+
+void Map::Resolve()
+{
+	for each (auto curVec in _vehicles)
+	{
+		auto rides = GetRidesByStart(0);
+		//TODO check if ride exists
+		//TODO choose ride
+		auto rideChosen = rides[0];
+		std::cout << rides[0] << std::endl;
+	}
 }
