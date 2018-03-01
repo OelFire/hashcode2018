7517149c9bafb4107abe83c2987868d8bb3575e4
Unknown <gaspard.thirion@epitech.eu>
2018-03-01 21:37:59 +0100

    bug rides

--- sources/Map.cpp
+++ sources/Map.cpp
@@ -97,7 +97,8 @@ void Map::GetRidesByStart(int start)
 
 void Map::Resolve()
 {
-	for (auto curVec : _vehicles)
+
+	for (auto it = _vehicles.begin(); it != _vehicles.end(); it++)
 	{
 		int time = 0;
 
@@ -108,15 +109,13 @@ void Map::Resolve()
 			GetRidesByStart(time);
 		}
 
-		std::cout << _map.size() << std::endl;
-		std::cout << _map[0]->getRideNumber() << std::endl;
-	
-		
-		
+		//std::cout << _map.begin()->second->getRideNumber() << std::endl;
 		//TODO choose ride
 
-		curVec.addRide(_map[0]);
+		it->addRide(_map.begin()->second);
 		//TODO add ride in curVec
 		//TODO remove ride from _rides
+		_rides.erase(_rides.begin() + _map.begin()->first);
+		it->display();
 	}
 }
--- sources/main.cpp
+++ sources/main.cpp
@@ -14,5 +14,6 @@ int main(int argc, char **argv) {
 
     map.parse();
 	map.Resolve();
+	std::cin;
     return 0;
 }
