6bee8519fdf06ca869918217499c580daff64dfe
mickael <mickael.bourgois@epitech.eu>
2018-03-01 20:05:47 +0100

    display vehicule rides

--- sources/Vehicle.cpp
+++ sources/Vehicle.cpp
@@ -2,8 +2,17 @@
 // Created by Vincent PICOT on 01/03/2018.
 //
 
+#include <iostream>
 #include "Vehicle.hpp"
 
 void Vehicle::addRide(std::shared_ptr<Ride> &ride) {
     _rides.push_back(ride);
+}
+
+void Vehicle::display() {
+	std::cout << _rides.size();
+	for (std::vector<std::shared_ptr<Ride>>::iterator it = _rides.begin(); it != _rides.end(); it++) {
+		std::cout << " " << (*it)->getId();
+	}
+	std::cout << std::endl;
 }
